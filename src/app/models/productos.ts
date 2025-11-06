f0317f5eb5be8816442ce982b4d14ed32c52e6d4
angeldzz <pintodiazangel@gmail.com>
2025-11-06 11:27:49 +0100

    otra sintaxis para el modelo

--- src/app/models/productos.ts
+++ src/app/models/productos.ts
@@ -1,12 +1,11 @@
 export class Producto{
     //las propiedades para que sean accesibles deben ser publicas
-    public nombre: string;
-    public imagen: string;
-    public precio: number;
+    
     //vamos a crear un constructor con parametros para que reciba la info de las propiedades directamente
-    constructor(name:string, image: string, price:number){
-        this.nombre = name;
-        this.imagen = image;
-        this.precio = price;
+    constructor(
+        public nombre: string,
+        public imagen: string,
+        public precio: number,
+    ){
     }
 }
